d2c36b55722c5038d9f622bc1d6e6a121a4d5c33
anurath <anurathwaghmode2018@gmail.com>
2024-12-27 14:26:52 +0530

    file patch

--- ecom/src/navBarPages/Login.jsx
+++ ecom/src/navBarPages/Login.jsx
@@ -1,19 +1,25 @@
+
+import Flipkart from '../assets/flipkart.png'
 import './Login.css'
-export default function Login(){
-    return(
+export default function Login() {
+
+    return (
         <div className="Login">
             <div className="sideLogin">
-                <h2>Login</h2>
-                <h4>Get access to your Orders, Wishlist and Recommendations</h4>
+                <div className='loginInfo'>
+                    <h2>Login</h2>
+                    <h4>Get access to your Orders, Wishlist and Recommendations</h4>
+                </div>
+                <img id='FlipkartImage' src={Flipkart} alt="Image Missing" />
             </div>
-           <div className="userLogin">
-            <form action="">
-                <input id='inp' type="text" placeholder="Enter Email/Mobile Number" />
-                <p className='terms'>By continuing, you agree to Flipkart's Terms of Use and Privacy Policy.</p>
-                <button>Request OTP</button>
-            </form>
-            <a href="/"><p className='resister'>New to Flipkart? Create an account</p></a>
-           </div>
+            <div className="userLogin">
+                <form action="">
+                    <input id='inp' type="text" placeholder="Enter Email/Mobile Number" />
+                    <h5 className='terms'>By continuing, you agree to Flipkart's Terms of Use and Privacy Policy.</h5>
+                    <button>Request OTP</button>
+                </form>
+                <a href="/"><h5 className='resister'>New to Flipkart? Create an account</h5></a>
+            </div>
         </div>
     )
 }
